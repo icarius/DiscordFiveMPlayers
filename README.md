f7bcb0702b2aba0b5b99040154ee57c161b78949
Théo CHASSEUR <tchasseur@monkeyfactory.fr>
2021-09-26 23:38:38 +0200

    First commit of this project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Discord Players FiveM
+
+Small nodeJS script allowing to change the name of a channel with the number of current players playing on your server!
+
+You just have to configure a bot:
+
+1. Invite the bot to your server
+
+2. Enter the bot token in the script
+
+3. Enter the FiveM detail url of your ex server (https://servers.fivem.net/servers/detail/id)
+
+4. Enter the identifier of the channel where you want the number of players to appear (if you do not know how to copy the identifier, you must put yourself in developer mode on discord and right click on the channel then copy the identifier )
---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const moment = require('moment');
+const cron = require('node-cron');
+const puppeteer = require('puppeteer');
+const Discord = require('discord.js');
+
+const app = express()
+app.set('view engine', 'pug');
+moment.locale('fr');
+
+const TOKEN_BOT = 'TOKEN BOT HERE';
+const URL_FIVEM = 'URL FIVEM DETAIL SERVER HERE EXAMPLE : https://servers.fivem.net/servers/detail/id';
+const ID_CHANNEL_PLAYERS = 'ID CHANNEL TO CHANGE NAME OF THIS';
+
+const PORT = process.env.PORT || 3000;
+
+async function getInfos() {
+    try {
+
+        const client = new Discord.Client({intents: [Discord.Intents.FLAGS.GUILDS]});
+
+        client.on("ready", async () => {
+            console.log('BOT DISCORD CONNECTED AND STARTED...');
+            const browser = await puppeteer.launch({
+                headless: true,
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            });
+            const page = await browser.newPage();
+
+            await page.goto(URL_FIVEM);
+
+            console.log('INFORMATIONS OF SERVER RECEIVED...');
+            const element = await page.$('.players-count');
+            const text = await (await element.getProperty('textContent')).jsonValue();
+            client.channels.fetch(ID_CHANNEL_PLAYERS).then(channel => {
+                console.log('SET NEW NAME WITH PLAYERS ON CHANNEL NAME...');
+                channel.setName('Joueurs:' + text.replace('group', ''));
+            });
+            await browser.close();
+
+        });
+        client.login(TOKEN_BOT);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+app.get('/', async (req, res) => { //get method
+    res.render('index');
+});
+
+app.get('/health', (req, res) => {
+    res.status(200).send();
+});
+
+app.listen(PORT, async () => {
+    console.log(`Server running on port ${PORT}`);
+    await getInfos();
+});
+
